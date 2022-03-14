390c1cac551a0314223ccec7b55d015c528dc3f1
Rocky Wang <55059833+rockywng@users.noreply.github.com>
2022-03-14 09:23:05 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,40 +18,45 @@ import requests
 import os
 
 app = Flask(__name__)
+# open model and vectorizer
 model = pickle.load(open("app/models/comment_classifier5.sav", "rb"))
 vectorizer = pickle.load(open("app/models/vectorizer5.sav", "rb"))
 
+# video unavailable site pattern, to be used for link verification
 pattern = '"playabilityStatus":{"status":"ERROR","reason":"Video unavailable"'
 
+# check if a given link is valid
 def validate_link(link):
-# check if link is actually a youtube link
-    # https://www.youtube.com/watch?v=v8o_hA2eMzI
+    # check if link does not follow standard youtube link formatting
     if not (link[0:32] == "https://www.youtube.com/watch?v="):
-        #print("linkfail")
         return False         
     request = requests.get(link)
+    # check if the video unavailable pattern appears, if True then the link is not valid
     return False if pattern in request.text else True
 
 def scrape_predict(link):
     data=[]
+    # set scraper options for Heroku
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.add_argument("--mute-audio")
     options.add_argument("--disable-gpu")
+    # begin scraping with path to Heroku chromedriver
     with Chrome(executable_path=("/app/.chromedriver/bin/chromedriver"), options=options) as driver:
         wait = WebDriverWait(driver,15)
         driver.get(str(link))
 
         for item in range(4): 
             wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-            #time.sleep(15)
-
+            
         for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
             data.append(comment.text)
+            
     clean_features = []
     df = pd.DataFrame(data, columns=['comment'])
+    # clean comments collected 
     for index, row in df.iterrows():
         clean = re.sub(r'\W', ' ', row['comment'])
         # remove single chars
@@ -65,10 +70,13 @@ def scrape_predict(link):
         # make all lowercase
         clean = clean.lower()
         clean_features.append(clean)
+    # use vectorizer to transform comments to appropriate input format
     clean_features = vectorizer.transform(clean_features).toarray()
+    # make predictions
     val = model.predict(clean_features)
     pos = 0
     neg = 0
+    # count positive and negative labels
     for i in range(len(val)):
         if (val[i] == 'Positive'):
             pos += 1
@@ -81,9 +89,6 @@ def scrape_predict(link):
     print(neg)
     return rat
 
-def zscore(mean, sd, val):
-    return (val - mean)/sd
-
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -91,28 +96,22 @@ def home():
 @app.route('/', methods=["GET", "POST"])
 def predict():
     link = request.form['entry']
+    # inform user if link is not valid
     if not (validate_link(link)):
-        #flash("i got bad input bro")
         return render_template("index.html", prediction_text = 'Please enter a valid video link of the form https://www.youtube.com/watch?v=VIDEOID')
-    print("got link!")
+    # make predictions if link is valid
     rat = scrape_predict(str(link))
-    print("done scrape")
-    #rat = scrape_predict("https://www.youtube.com/watch?v=AK3HxCOZZ6w")
+    # handle edge cases (-1, 0, 1)
     if (rat == -1):
         return render_template("index.html", prediction_text = "The model failed to identify sentiment for this video.")
     if (rat == 1):
         return render_template("index.html", prediction_text = "The comments on this video were almost 100% positive.")
     if (rat == 0):
         return render_template("index.html", prediction_text = "The comments on this video were almost 0% positive.")
-    print("predictions made!")
+    # convert from float to int and multiply by 100 to get percentage value
     percent = int(rat * 100)
+    # return percent to user
     return render_template("index.html", prediction_text = "The comments on this video were " + str(percent) + "% positive.")
-
-# WORKING BASE CASE HERE!
-""" @app.route('/', methods=["GET", "POST"])
-def home():
-    rat = scrape_predict("https://www.youtube.com/watch?v=AK3HxCOZZ6w")
-    return str(rat) """
 
 if __name__ == '__main__':
     #app.debug = True
